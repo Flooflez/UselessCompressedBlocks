fb13eaf54ea4b7db426b77667c31273df45a9af0
Flooflez <maxmyta@gmail.com>
2023-05-23 16:01:50 +0800

    fixed incorrect colours in textures

--- src/main/java/net/useless/compressed/blocks/ItemInit.java
+++ src/main/java/net/useless/compressed/blocks/ItemInit.java
@@ -5,7 +5,6 @@ import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
--- src/main/java/net/useless/compressed/blocks/blocks/CompressedCraftingTable.java
+++ src/main/java/net/useless/compressed/blocks/blocks/CompressedCraftingTable.java
@@ -1,10 +1,30 @@
 package net.useless.compressed.blocks.blocks;
 
-import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.CraftingTableBlock;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.stat.Stats;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
-public class CompressedCraftingTable extends Block {
+public class CompressedCraftingTable extends CraftingTableBlock {
+
     public CompressedCraftingTable() {
         super(Settings.copy(Blocks.CRAFTING_TABLE).resistance(3f));
+    }
+
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        if (world.isClient) {
+            return ActionResult.SUCCESS;
+        } else {
+            player.openHandledScreen(state.createScreenHandlerFactory(world, pos));
+            player.incrementStat(Stats.INTERACT_WITH_CRAFTING_TABLE);
+            System.out.println("test");
+            return ActionResult.CONSUME;
+        }
     }
 }
