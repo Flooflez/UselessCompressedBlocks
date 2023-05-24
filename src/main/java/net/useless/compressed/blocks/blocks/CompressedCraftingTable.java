9c495a412526a031008d0b403543452542b4d908
Flooflez <maxmyta@gmail.com>
2023-05-24 15:32:06 +0800

    fixed instant closing GUI and added recipes

--- src/main/java/net/useless/compressed/blocks/blocks/CompressedCraftingTable.java
+++ src/main/java/net/useless/compressed/blocks/blocks/CompressedCraftingTable.java
@@ -4,12 +4,17 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.CraftingTableBlock;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.stat.Stats;
+import net.minecraft.screen.CraftingScreenHandler;
+import net.minecraft.screen.NamedScreenHandlerFactory;
+import net.minecraft.screen.ScreenHandlerContext;
+import net.minecraft.screen.SimpleNamedScreenHandlerFactory;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 
 public class CompressedCraftingTable extends CraftingTableBlock {
 
@@ -17,14 +22,4 @@ public class CompressedCraftingTable extends CraftingTableBlock {
         super(Settings.copy(Blocks.CRAFTING_TABLE).resistance(3f));
     }
 
-    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
-        if (world.isClient) {
-            return ActionResult.SUCCESS;
-        } else {
-            player.openHandledScreen(state.createScreenHandlerFactory(world, pos));
-            player.incrementStat(Stats.INTERACT_WITH_CRAFTING_TABLE);
-            System.out.println("test");
-            return ActionResult.CONSUME;
-        }
-    }
 }
